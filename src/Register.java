ea62871d0049bc8379f52fea19d541b2b7ec6b5d
shishir54234 <shahishishir4@gmail.com>
2023-04-21 15:19:38 +0530

    heyy

--- src/Register.java
+++ src/Register.java
@@ -5,7 +5,9 @@ public class Register {
     public static void main(){
         // Prepare a parameterized SELECT query
         Scanner sc=new Scanner(System.in);
+        System.out.println("Please give us your UserID: ");
         String userId=sc.next();
+        System.out.println("Please give us your password : ");
         String password=sc.next();
         String sql = "SELECT * FROM patients WHERE user_id = ? AND password = ?";
         try {
@@ -20,7 +22,7 @@ try (// Connect to the database
             PreparedStatement stmt = conn.prepareStatement(sql);
             stmt.setString(1, userId);
             stmt.setString(2, password);
-
+            
             // Execute the query and get the result set
             ResultSet rs = stmt.executeQuery();
 
@@ -30,6 +32,26 @@ try (// Connect to the database
                 // we use the login class
 
             } else {
+                sql = "INSERT INTO patients_login (username, password) VALUES (?, ?)";
+                stmt = conn.prepareStatement(sql);
+                stmt.setString(1, userId);
+                stmt.setString(2, password);
+                stmt.executeUpdate();
+                System.out.println("Please give us your name: ");
+                String name=sc.next();
+                System.out.println("Please give us your age: ");
+                Integer age=sc.nextInt();
+                System.out.println("Please give us your gender: ");
+                String gender=sc.next();
+
+                sql = "INSERT INTO patients (patientID, patientName , age, gender) VALUES (?, ?, ?, ?, ?, ?)";
+                stmt = conn.prepareStatement(sql);
+                stmt.setString(1, userId);
+                stmt.setString(2, name);
+                stmt.setInt(3, age);
+                stmt.setString(4, gender);
+
+                stmt.executeUpdate();
                 System.out.println("User ID and password have been registered");
             }
         } catch (SQLException e) {
